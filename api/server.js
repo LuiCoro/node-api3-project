5df2d7a1eb5a18b712c460f3d31e628a67448c9c
LuiCoro <luiacorona029@icloud.com>
2021-11-02 15:26:24 -0700

    'All Routes hoked up'

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const server = express();
+const userRouter = require('./users/users-router')
 
 const {logger} = require('./middleware/middleware')
 
@@ -9,6 +10,8 @@ const {logger} = require('./middleware/middleware')
 // global middlewares and the user's router need to be connected here
 server.use(express.json())
 server.use(logger)
+
+server.use('/api/users', userRouter)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
