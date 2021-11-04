a7252305e5ee2ecb710ace2a3d8d166bc900cf74
LuiCoro <luiacorona029@icloud.com>
2021-11-03 17:29:16 -0700

    'All Routes hoked up'

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -2,6 +2,7 @@ const Users = require('../users/users-model')
 
 function logger(req, res, next) {
   // DO YOUR MAGIC
+
   console.log(
       `[${new Date().toISOString()}] ${req.method}
       to ${req.url} from ${req.get('Origin')}`
@@ -9,18 +10,49 @@ function logger(req, res, next) {
   next()
 }
 
-function validateUserId(req, res, next) {
+async function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+
+  try{
+    const user = await Users.getById(req.params.id)
+
+    if(!user) {
+      next({ status: 404, message: 'user not found'})
+    } else {
+      req.user = user
+      next()
+    }
+  } catch (err) {
+    res.status(500).json({ message: 'problem finding user'})
+  }
+
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+
+  const {name} = req.body
+  if(!name || !name.trim()) {
+    res.status(400).json({ message: 'missing required name field',})
+  } else {
+    req.name = name.trim
+    next()
+  }
+
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+
+  const {text} = req.body
+  if(!text || !text.trim()) {
+    res.status(400).json({message: "missing required text field",})
+  } else {
+    req.text = text.trim()
+    next()
+  }
 }
 
 // do not forget to expose these functions to other modules
 
-module.exports = {logger}
+module.exports = {logger, validateUserId, validateUser , validatePost}
